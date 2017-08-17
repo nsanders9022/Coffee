e242e77178c0340c1e8ff1b3a1ccd8325cd60a0d
Nicole Sanders <nsanders9022@gmail.com>
2017-08-16 21:31:17 -0700

    add return statement for if the coffee machine is not found

--- Coffee.js
+++ Coffee.js
@@ -156,6 +156,8 @@ function DistanceToCoffee(numRows, numColumns, deskLocation, coffeeLocations, wa
   if (solve(id, numRows, numColumns)) {
     return pathArray.length;
   }
+  return -1;
+    
 }
 
  // DistanceToCoffee(3, 4, [2,1], [[1,3],[3,2]], [[2,2],[2,3],[3,1]]);
